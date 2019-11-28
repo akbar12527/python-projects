a4189084de0062e44a55bde7e53234937b799b67
akbarash22 <akbar12525@gmail.com>
2019-11-28 15:38:20 +0530

    adding label

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import tkinter as tk
 import os
 import time
 
-root=tk.Tk()
+root = tk.Tk()
 
 
-
-button = tk.Button(root,text='click me')
+button = tk.Button(root, text='click me')
 button.pack()
-
+label = tk.Label(root, text='heloo world')
+label.pack()
 root.maintloop()
